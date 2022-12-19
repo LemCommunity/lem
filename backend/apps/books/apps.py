3b639717deb2fd11be1cbae8f0a9d774906e80c3
Matt <mefpef@gmail.com>
2022-12-19 19:50:56 +0100

    minor changes, add missing app files

--- backend/apps/books/apps.py
+++ backend/apps/books/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BooksConfig(AppConfig):
+class ForumConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "backend.apps.books"
