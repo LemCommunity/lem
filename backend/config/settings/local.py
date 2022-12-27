cb1a1ca70c3719a94096ef981d31799cabb8df07
radthenone <krzyzak.codes@gmail.com>
2022-12-27 06:21:26 +0100

    add martor to APPS

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -37,7 +37,10 @@ EMAIL_BACKEND = env(
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
-INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
+INSTALLED_APPS += [  # noqa F405
+    "debug_toolbar",
+    "martor",
+]
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
 MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
