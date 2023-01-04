b55c23f604974aedf05c0865694f44ff8b516120
aruzo <dawid12391111@gmail.com>
2023-01-05 00:03:16 +0100

    Configure static files hosting (#45)

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -130,9 +130,7 @@ MIDDLEWARE = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
 STATIC_ROOT = str(BACKEND_DIR / "staticfiles")
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = "/static/"
-# https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = [str(BACKEND_DIR / "static")]
+STATIC_URL = "/api/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
