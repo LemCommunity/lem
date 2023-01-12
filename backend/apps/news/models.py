1642563ce1f86d805ee84e2717a4eff0f209ea33
raf-lim <raf.sukiennik@gmail.com>
2023-01-13 00:18:27 +0100

    add validator for title and tag, add translations

--- backend/apps/news/models.py
+++ backend/apps/news/models.py
@@ -3,9 +3,15 @@
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
-from django.core.validators import MaxLengthValidator, MinLengthValidator
+from django.core.validators import (
+    MaxLengthValidator,
+    MinLengthValidator,
+    RegexValidator,
+    validate_slug,
+)
 from django.db import models
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 from django_extensions.db.fields import (
     AutoSlugField,
     CreationDateTimeField,
@@ -26,7 +32,9 @@ class UserActionTimestampedMixin(models.Model):
     created = CreationDateTimeField()
     modified = ModificationDateTimeField()
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name=_("user")
+    )
 
 
 class PolymorphicRelationship(models.Model):
@@ -44,6 +52,10 @@ class PolymorphicRelationship(models.Model):
 class Highlight(UserActionTimestampedMixin, PolymorphicRelationship):
     """Highlight model"""
 
+    class Meta:
+        verbose_name = _("highlight")
+        verbose_name_plural = _("highlights")
+
     highlight = models.BooleanField(default=False)
 
     def __str__(self):
@@ -56,9 +68,16 @@ class Tag(UserActionTimestampedMixin, PolymorphicRelationship):
     class Meta:
         ordering = ["name"]
 
-    name = models.CharField(unique=True, max_length=50)
-    # TODO add validators
-
+    name = models.CharField(
+        unique=True,
+        max_length=50,
+        validators=[
+            MinLengthValidator(3, message=_("Min length is 3 characters.")),
+            MaxLengthValidator(200, message=_("Max length is 200 characters.")),
+            validate_slug,
+        ],
+        help_text=_("Must be unique, only alphanumeric allowed"),
+    )
     slug = AutoSlugField(populate_from="name")
 
     def __str__(self) -> str:
@@ -77,49 +96,46 @@ class Comment(UserActionTimestampedMixin, PolymorphicRelationship):
         return self.body[:20]
 
 
-# draft for further development depending on needs
-class NewsManager(models.Manager):
-    def get_queryset(self):
-        """Returns queryset with published news objects."""
-        return self.all_objects().filter(is_published=True)
-
-    def all_objects(self):
-        """Returns queryest with all news objects (default get_queryset)"""
-        return super().get_queryset()
-
-    def inactive(self):
-        """Returns queryset with not published news objects"""
-        return self.all_objects().filter(is_published=False)
-
-
 class News(UserActionTimestampedMixin):
+    """News model"""
+
     class Meta:
-        verbose_name_plural = "News"
+        verbose_name_plural = _("news")
 
     title = models.CharField(
         null=False,
         blank=False,
         max_length=200,
         validators=[
-            MinLengthValidator(3, message="Title must be of at least 3 characters."),
-            MaxLengthValidator(
-                200, message="Title cannot be longer then 200 characters."
+            MinLengthValidator(3, message=_("Min length is 3 characters.")),
+            MaxLengthValidator(200, message=_("Max length is 200 characters.")),
+            RegexValidator(
+                regex=r"^[\w\.\'\" ]+$",
+                message=_("Only alphanumeric, ', \", dot and space allowed."),
             ),
-            # TODO add RegexValidator
         ],
+        help_text=_(
+            "Min lenght of 3 chars, max length of 200 chars, only alphanumeric, ', \", dot and space allowed"
+        ),
+        verbose_name=_("news title"),
     )
     slug = AutoSlugField(populate_from=["title"])
 
-    body = models.TextField(null=False, blank=False)
+    body = models.TextField(null=False, blank=False, verbose_name=_("news body"))
     # TODO add body validator, html safety issue?
 
-    is_published = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=False, verbose_name=_("is published"))
+    allow_highlights = models.BooleanField(
+        null=False, blank=False, default=True, verbose_name=_("allow highlights")
+    )
+    allow_likes = models.BooleanField(
+        null=False, blank=False, default=True, verbose_name=_("allow likes")
+    )
+    allow_comments = models.BooleanField(
+        null=False, blank=False, default=True, verbose_name=_("allow comments")
+    )
 
-    allow_highlights = models.BooleanField(null=False, blank=False, default=True)
-    allow_likes = models.BooleanField(null=False, blank=False, default=True)
-    allow_comments = models.BooleanField(null=False, blank=False, default=True)
-
-    objects = NewsManager()
+    # objects = NewsManager()
 
     # files = GenericRelation(File, related_query_name="news")  # interview text
     # images = GenericRelation(Image, related_query_name="news")  # news image
