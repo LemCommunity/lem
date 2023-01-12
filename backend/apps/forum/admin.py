de5e200527900b72bde4397f12ec9981e842733d
jasioCode <101008688+jasiuSYN@users.noreply.github.com>
2023-01-12 23:23:59 +0100

    Add slug to readonly_fields

--- backend/apps/forum/admin.py
+++ backend/apps/forum/admin.py
@@ -20,6 +20,7 @@ class PostAdmin(admin.ModelAdmin):
     readonly_fields = (
         "created_at",
         "updated_at",
+        "slug",
     )
     search_fields = ("title", "author__name")
     list_filter = ("category",)
